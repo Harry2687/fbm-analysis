06c1545f32ad3de69847283c2fbfef7215e9f0c4
Harry2687 <harry.zhong2687@gmail.com>
2024-03-29 11:01:24 +0800

    Added word search

--- functions/fbmessenger.py
+++ functions/fbmessenger.py
@@ -3,6 +3,10 @@ import json
 import pandas as pd
 import datetime as dt
 
+def ms_convert_timestamp(timestamp_ms):
+    timestamp = dt.datetime.fromtimestamp(timestamp_ms/1000)
+    return timestamp
+
 def ms_import_data(directory):
     data_file_names = os.listdir(directory)
     data_files = [os.path.join(directory, data_file_name) for data_file_name in data_file_names]
@@ -15,8 +19,19 @@ def ms_import_data(directory):
         json_data = pd.json_normalize(data['messages'])
         messenger_data = pd.concat([messenger_data, json_data])
 
+    messenger_data = messenger_data[[
+        'sender_name',
+        'timestamp_ms',
+        'content'
+    ]].dropna().sort_values('timestamp_ms', ascending=False)
+
+    messenger_data['timestamp'] = messenger_data['timestamp_ms'].apply(ms_convert_timestamp)
+
+    messenger_data = messenger_data[[
+        'sender_name',
+        'timestamp',
+        'content'
+    ]]
+
     return messenger_data
 
-def ms_convert_timestamp(timestamp_ms):
-    timestamp = dt.datetime.fromtimestamp(timestamp_ms/1000)
-    return timestamp
