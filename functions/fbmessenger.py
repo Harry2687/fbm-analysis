f18e9d3be8bb4cd2d8c3ee9fb41cae8073ead7b9
Harry2687 <harry.zhong2687@gmail.com>
2024-04-01 12:37:50 +0800

    Added conversations based on sent times

--- functions/fbmessenger.py
+++ functions/fbmessenger.py
@@ -3,10 +3,6 @@ import json
 import pandas as pd
 import datetime as dt
 
-def ms_convert_timestamp(timestamp_ms):
-    timestamp = dt.datetime.fromtimestamp(timestamp_ms/1000)
-    return timestamp
-
 def ms_import_data(directory):
     data_file_names = os.listdir(directory)
     data_files = [os.path.join(directory, data_file_name) for data_file_name in data_file_names]
@@ -23,9 +19,9 @@ def ms_import_data(directory):
         'sender_name',
         'timestamp_ms',
         'content'
-    ]].dropna().sort_values('timestamp_ms', ascending=False)
+    ]].dropna().sort_values('timestamp_ms', ascending=True)
 
-    messenger_data['timestamp'] = messenger_data['timestamp_ms'].apply(ms_convert_timestamp)
+    messenger_data['timestamp'] = pd.to_datetime(messenger_data['timestamp_ms'], unit='ms')
 
     messenger_data = messenger_data[[
         'sender_name',
