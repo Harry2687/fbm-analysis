3c01253f6fdd95100b3893255899a503387adaf0
Harry2687 <harry.zhong2687@gmail.com>
2025-01-02 18:52:04 +0800

    moved lda code to preprocessing

--- modules/preprocessing.py
+++ modules/preprocessing.py
@@ -2,6 +2,7 @@ import os
 import json
 import re
 import pandas as pd
+import gensim as gs
 
 def ms_import_data(directory: str) -> pd.DataFrame:
     data_file_names = os.listdir(directory)
@@ -41,6 +42,117 @@ def remove_custom_stopwords(document: str, stopwords: list) -> str:
         document = re.sub(pattern, '', document).replace('  ', ' ')
     
     return document
+
+def lda_preprocess(dataframe: pd.DataFrame, content_col: str, cleaned_col: str, rm_stopwords: bool=True) -> pd.DataFrame:
+    dataframe[cleaned_col] = (
+        dataframe[content_col]
+        .str.lower()
+        .str.strip()
+        .str.replace('[^a-z\\s]', '', regex=True) # remove anything that isn't text or spaces
+        .str.replace('\\s{2,}', ' ', regex=True) # replace 2+ spaces with single space
+    )
+
+    chat_actions = [
+        'reacted to your message'
+    ]
+
+    # remove chat actions which aren't part of the conversation
+    dataframe = dataframe[
+        ~dataframe[cleaned_col]
+        .str.contains(
+            '|'.join(chat_actions)
+        )
+    ]
+
+    # stopwords which are not included in gensim
+    custom_stopwords = [
+        'u', 
+        'lmao', 
+        'lol', 
+        'ur', 
+        'like', 
+        'yea', 
+        'thats', 
+        'nah', 
+        'im', 
+        'yeh', 
+        'dont',
+        'yeah', 
+        'gonna', 
+        'didnt',
+        'idk',
+        'got',
+        'r',
+        'sure',
+        'come',
+        'stuff'
+        'k'
+    ]
+
+    if rm_stopwords:
+        # remove gensim and custom stopwords
+        dataframe[cleaned_col] = (
+            dataframe[cleaned_col]
+            .apply(gs.parsing.preprocessing.remove_stopwords)
+            .apply(remove_custom_stopwords, args=(custom_stopwords,))
+            .str.strip()
+            .str.replace('\\s{2,}', ' ', regex=True)
+        )
+
+    return dataframe
+
+def sender_wordcount(dataframe: pd.DataFrame, search_word: str) -> pd.DataFrame:
+    dataframe['clean_content_splitlist'] = (
+        dataframe['clean_content']
+        .str.split(' ')
+    )
+
+    chat_data_wcount = (
+        dataframe[['sender_name', 'clean_content_splitlist']]
+        .explode('clean_content_splitlist')
+        .query('clean_content_splitlist == @search_word')
+        .value_counts()
+        .reset_index()
+        .drop('clean_content_splitlist', axis=1)
+        .sort_values('count', ascending=False)
+    )
+
+    return chat_data_wcount
+
+def lda_getdocs(dataframe: pd.DataFrame, content_col: str, ts_col: str, conv_cutoff: int=600):
+    # calculate difference between each message
+    dataframe['time_diff'] = (
+        dataframe[ts_col]
+        .diff()
+        .fillna(pd.Timedelta(seconds=0))
+    )
+    dataframe['time_diff'] = (
+        dataframe['time_diff']
+        .dt.total_seconds()
+    )
+
+    # group dataframe into different conversations based on cutoff value
+    dataframe['new_conv'] = dataframe['time_diff'] > conv_cutoff
+    dataframe['conv_num'] = 'Conv ' + (
+        dataframe['new_conv']
+        .cumsum()
+        .astype(str)
+    )
+
+    # join together messages in the same coversation
+    conversations = (
+        dataframe
+        .groupby('conv_num')
+        [content_col]
+        .apply(lambda x: ' '.join(map(str, x)))
+        .str.strip()
+        .str.replace('\\s{2,}', ' ', regex=True)
+        .reset_index()
+    )
+
+    documents = conversations[content_col].tolist()
+
+    return documents
 
 def convert_to_txt(source: str, output: str, timestamp: bool=False):
     pd_data = ms_import_data(source)
